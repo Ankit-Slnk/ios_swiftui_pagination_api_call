8e8fcb198df2d19c86d356992c2b75131fe6e933
Ankit Solanki <ankit_ms95@yahoo.com>
2020-11-25 10:01:25 +0530

    Initial Commit

---
+++ ios-swiftui-pagination-api-call/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-swiftui-pagination-api-call
+//
+//  Created by Ankit Solanki on 25/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-swiftui-pagination-api-call/ios_swiftui_pagination_api_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftui_pagination_api_callApp.swift
+//  ios-swiftui-pagination-api-call
+//
+//  Created by Ankit Solanki on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftui_pagination_api_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
